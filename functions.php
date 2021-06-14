32e0dff5f3c8f349055f0294454b82f31ad0b365
Aditya Jain <vvasujain@gmail.com>
2021-06-14 23:08:29 +0530

    Added frontend, yet to be refined

--- functions.php
+++ functions.php
@@ -16,11 +16,27 @@ class FunctionsClass {
 		);
 
 		wp_enqueue_style(
-			'additional-style',
-			get_template_directory_uri() . '/assets/css/style.css',
+			'font-awesome-style',
+			'//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
 			array(),
-			filemtime( plugin_dir_path( __FILE__ ) . 'assets/css/style.css' )
+			'4.7.0',
+			false
 		);
+
+		wp_enqueue_style(
+			'roboto-font-style',
+			'//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i',
+			array(),
+			'1.0.0',
+			false
+		);
+
+//		wp_enqueue_style(
+//			'additional-style',
+//			get_template_directory_uri() . '/assets/css/style.css',
+//			array(),
+//			filemtime( plugin_dir_path( __FILE__ ) . 'assets/css/style.css' )
+//		);
 
 	}
 
