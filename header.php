f15d38073fd3899f7a27c9ef5746d9cea37182dc
Aditya Jain <vvasujain@gmail.com>
2021-06-29 23:55:56 +0530

    made custom post to be visible for the user

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php wp_head(); ?></title>
+	
 </head>
 <body <?php body_class(); ?>>
 <header class="site-header">
